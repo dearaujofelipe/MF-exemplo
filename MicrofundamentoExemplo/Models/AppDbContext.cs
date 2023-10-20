3440090ceddc6096fd5c021927a7e4e91baa881d
dearaujofelipe <mgdefelipe@outlook.com>
2023-10-20 15:43:36 -0300

    first

---
+++ MicrofundamentoExemplo/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MicrofundamentoExemplo.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+
+    }
+}
---
+++ MicrofundamentoExemplo/Models/Veiculo.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MicrofundamentoExemplo.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "obrigatorio informar nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "obrigatorio informar a placa")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "obrigatorio informar ano de fab")]
+
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "obrigatorio informar ano de modelo")]
+
+        public int AnoModelo { get; set; }
+    }
+}
