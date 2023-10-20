88452d8d392e8aa404dcb970f1fda388506f0f66
dearaujofelipe <mgdefelipe@outlook.com>
2023-10-20 18:38:48 -0300

    crud

--- MicrofundamentoExemplo/Models/Veiculo.cs
+++ MicrofundamentoExemplo/Models/Veiculo.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
 
 namespace MicrofundamentoExemplo.Models
 {
@@ -10,16 +11,20 @@ namespace MicrofundamentoExemplo.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "obrigatorio informar nome")]
+        [Display(Name = "Filme")]
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "obrigatorio informar a placa")]
+        [Display(Name = "Diretor")]
         public string Placa { get; set; }
 
         [Required(ErrorMessage = "obrigatorio informar ano de fab")]
+        [Display(Name = "Metacritic Score")]
 
         public int AnoFabricacao { get; set; }
 
         [Required(ErrorMessage = "obrigatorio informar ano de modelo")]
+        [Display(Name = "Meu Score")]
 
         public int AnoModelo { get; set; }
     }
